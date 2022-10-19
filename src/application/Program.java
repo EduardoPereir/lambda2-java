f3bc2ef6cfb8665f312fdaf7b055f8da8869316c
Eduardo Pereira Filho <eduardopereirafilho6@gmail.com>
2022-10-19 09:45:27 -0300

    Predicate Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,26 @@
 package application;
 
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        List<Product> list = new ArrayList<>();
 
+        list.add(new Product("Mouse", 54.90 ));
+        list.add(new Product("Monitor", 234.93 ));
+        list.add(new Product("Xbox", 1190.99 ));
+
+        list.removeIf(new ProductPredicate());
+
+        for ( Product p: list){
+            System.out.println(p);
+        }
 
     }
 }
